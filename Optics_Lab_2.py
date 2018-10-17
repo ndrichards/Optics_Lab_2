e283e880dca0bc87179f8af2769456d962843eb9
Lucas Ryan Jameson <34043011+lrj1197@users.noreply.github.com>
2018-10-17 17:57:44 -0400

    Add files via upload

--- Optics_Lab_2.py
+++ Optics_Lab_2.py
@@ -42,33 +42,108 @@ def fit(T_measured,Resistance,p0):
 dataset = []
 for f in os.listdir('/Users/lucas/Documents/Optics/Lab_Notes/Optics_Lab_2-master/'):
     data = genfromtxt('/Users/lucas/Documents/Optics/Lab_Notes/Optics_Lab_2-master/%s' % (f,))
+    print('%s',(f,))
     dataset.append(data[:,0])
     dataset.append(data[:,1])
 dataset = pd.DataFrame(dataset)
 dataset = dataset.T
-x = genfromtxt('/Users/lucas/Documents/Optics/Lab_Notes/Optics_Lab_2-master/outdoors.txt')
-x[:,]
-
-dataset[0] = dataset['outdoors_']
-
-
-
-
-
-
-
-
-
+#%s ('argon (2).txt',) 0,1
+#%s ('flourescent (2).txt',) 2,3
+#%s ('incandescent (2).txt',) 4,5
+#%s ('mercury (3).txt',) 6,7
+#%s ('outdoors.txt',) 8,9
+dataset['out_lamda'] = dataset[9]
+dataset['out_c'] = dataset[8]
+for i in range(9):
+    del dataset[i]
+dataset.columns
+dataset
 
 
 
+
+plt.plot(dataset['Hg_c'][400:2000], dataset['Hg_lamda'][400:2000])
+
+
+from scipy.signal import argrelextrema
+dataset.columns
+n=150
+m=10 # number of points to be checked before and after
+# Find local peaks
+#df['min'] = df.iloc[argrelextrema(df.data.values, np.less_equal, order=n)[0]]['data']
+dataset['max_Hg'] = dataset.iloc[argrelextrema(dataset['Hg_lamda'].values, greater_equal, order=n)[0]]['Hg_lamda']
+dataset['max_Hg'] = dataset['max_Hg'].apply(lambda x: nan if x < 10000 else x)
+dataset['max_Ar'] = dataset.iloc[argrelextrema(dataset['Ar_lamda'].values, greater_equal, order=m)[0]]['Ar_lamda']
+dataset['max_Ar'] = dataset['max_Ar'].apply(lambda x: nan if x < 10000 else x)
+dataset['max_flo'] = dataset.iloc[argrelextrema(dataset['flo_lamda'].values, greater_equal, order=n)[0]]['flo_lamda']
+dataset['max_flo'] = dataset['max_flo'].apply(lambda x: nan if x < 10000 else x)
+dataset['max_inc'] = dataset.iloc[argrelextrema(dataset['inc_lamda'].values, greater_equal, order=n)[0]]['inc_lamda']
+dataset['max_inc'] = dataset['max_inc'].apply(lambda x: nan if x < 10000 else x)
+dataset['max_out'] = dataset.iloc[argrelextrema(dataset['out_lamda'].values, greater_equal, order=n)[0]]['out_lamda']
+dataset['max_out'] = dataset['max_out'].apply(lambda x: nan if x < 10000 else x)
+
+dataset
+plt.scatter(dataset['out_c'][:], dataset['max_out'], c='g')
+plt.plot(dataset['out_c'][:], dataset['out_lamda'][:],c='b',label = 'out')
+plt.scatter(dataset['inc_c'][:], dataset['max_inc'], c='g')
+plt.plot(dataset['inc_c'][:], dataset['inc_lamda'][:],c='b',label = 'inc')
+
+
+# Plot results
+
+plt.scatter(dataset['Hg_c'][:], dataset['max_Hg'], c='g')
+plt.plot(dataset['Hg_c'][:], dataset['Hg_lamda'][:],c='b',label = 'Hg')
+plt.scatter(dataset['Ar_c'][:], dataset['max_Ar'], c='r')
+plt.plot(dataset['Ar_c'][:], dataset['Ar_lamda'][:],c = 'k',label = 'Ag')
+plt.scatter(dataset['flo_c'][:], dataset['max_flo'], c='c')
+plt.plot(dataset['flo_c'][:], dataset['flo_lamda'][:],c = 'y',label = 'Flo')
+plt.legend()
+plt.xlabel("Lambda (nm)")
+plt.ylabel("Counts")
+plt.savefig("/Users/lucas/Documents/Optics_plot_0.png")
+plt.show()
 
 dataset
 dataset
-plt.plot(dataset[0], dataset[1])
+index = 2
+for i in range(0,9,2):
+    plt.plot(dataset[i][400:2000], dataset[i+1][400:2000])
+
 
 p0 = (1.,1.,1.)
 args, err, chi = = fit(T_measured,Resistance,p0)
+
+
+
+#Slice the data and get the local maximums for eah signal
+ra2261 = dataset['Ra_226 pure signal'][8:16]
+
+
+#get the max for each peak
+max_ra226_1 = np.max(dataset[index][1400:2000])
+#max_ra226_2 = np.max(ra2262)
+
+
+
+#Find which channel those maximums occur at
+where(dataset[1] == max(dataset[index][1400:2000]))
+cs2 = int(np.where(dataset['Cs_137 pure signal'] == max_cs137_2)[0])
+
+#ra4,ra3,ra2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Look for Tc
 R = L(dataset['Temperature'], *args)
